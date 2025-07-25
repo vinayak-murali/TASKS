ea91c9e24fa2b16a77f40a3f913e6efdf2e4d848
Vinayak Muraleedharan <vinayak90tcr@gmail.com>
2025-07-25 23:22:26 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,10 @@
+package Assignment;
+
+import org.example.ChromeDriver;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+    }
+}
